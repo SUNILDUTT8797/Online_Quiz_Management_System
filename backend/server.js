ec55e6041021400613e9cd8525789e9719580b4c
SUNILDUTT8797 <sdutt8797@gmail.com>
2025-12-14 20:18:21 +0530

    Add DB_PORT support for Aiven MySQL

--- backend/server.js
+++ backend/server.js
@@ -49,6 +49,7 @@ const connectDB = async () => {
   try {
     const conn = await mysql.createConnection({
       host: process.env.DB_HOST,
+      port: process.env.DB_PORT || 3306,
       user: process.env.DB_USER,
       password: process.env.DB_PASS,
       database: process.env.DB_NAME
